2f93876af52366b8819ca0de85af4466046ad6ea
Takuya Miyamoto <64185295+tm0605@users.noreply.github.com>
2024-01-11 14:15:30 +1000

    Changed from js to ts

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
