e238becfa8cbc774bc17d9d49f79258724696cb0
Takuya Miyamoto <64185295+tm0605@users.noreply.github.com>
2024-01-20 11:15:39 +1000

    Deleted ts file

--- server/routes/flightRoutes_ts.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import getFlights from '../controllers/flightController';
-console.log('flightRoutes')
-const router = Router();
-
-router.get('/flights', getFlights); // Route to get flights
-
-export default router;
