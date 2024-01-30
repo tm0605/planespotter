470245e2ff86a09c223ba8b2bbfeb73a0fa513ca
Takuya Miyamoto <64185295+tm0605@users.noreply.github.com>
2024-01-31 04:21:53 +1000

    added app routing

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
 import flightRoutes from './routes/flightRoutes.js';
@@ -7,6 +9,7 @@ dotenv.config();
 
 const app = express();
 const PORT = parseInt(process.env.PORT, 10) || 5000; // Backend port
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(cors()); // Enable CORS for frontend access
 app.use(express.json());
@@ -15,10 +18,13 @@ app.use('/api/flights', flightRoutes);
 
 app.use('/api/photo', locationRoutes);
 
-// app.get('/', (req: Request, res: Response) => {
-app.get('/', (_req, res) => {
-    res.send('Backend is Running!');
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
