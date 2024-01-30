61c6241936855a0a339cb225796aa493f387e6ef
Takuya Miyamoto <64185295+tm0605@users.noreply.github.com>
2024-01-31 04:20:46 +1000

    removed console log

--- server/routes/flightRoutes.js
+++ server/routes/flightRoutes.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import getFlightsAll from '../controllers/flightController.js';
-console.log('flightRoutes');
 const router = Router();
 
 router.get('/all', getFlightsAll); // Route to get flights
